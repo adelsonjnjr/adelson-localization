ed185cb74c9a3235968724ac52ef19a92df87630
Jean Junior Adelson <adelsonjnjr+adelson@gmail.com>
2025-12-13 15:04:39 -0500

    feat: Make the helper functions deepMerge and strictDeepMerge available for the package users.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useLanguage } from "./useLanguage";
 export type { UseLanguageConfig, UseLanguageReturn } from "./useLanguage";
 export { stringHelpExtensions } from "./stringHelpers";
+export { deepMerge, strictDeepMerge } from "./objectHelpers";
